8f957dd83ef4073e092d326651a242aa37d0a5a4
ZiyangLiu <liusg@mail.ustc.edu.cn>
2017-12-29 21:40:55 +0800

    测试SmartGit功能

---
+++ SmartGitTest/SmartGitTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SmartGitTest
+//
+//  Created by I ce d ra gon 刘 on 2017/12/29.
+//  Copyright © 2017年 I ce d ra gon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
